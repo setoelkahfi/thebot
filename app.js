06b1348bcd2c61d3acc3f867335d81a864596d9f
Seto Elkahfi <setoelkahfi@gmail.com>
2017-10-17 17:11:47 +0700

    Exercise 3: Making the Bot Smarter with Language Understanding (Node.js)

--- app.js
+++ app.js
@@ -28,23 +28,60 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by eching each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, [
+var bot = new builder.UniversalBot(connector, (session) => {
+    session.endDialog(`I'm sorry, I did not understand '${session.message.text}'.\nType 'help' to know more about me :)`);
+});
+
+bot.dialog('Help',
+(session, args, next) => {
+    session.endDialog(`I'm the help desk bot and I can help you create a ticket.\n` +
+        `You can tell me things like _I need to reset my password_ or _I cannot print_.`);
+}
+).triggerAction({
+    matches: 'Help'
+});
+
+bot.dialog('SubmitTicket', [
     (session, args, next) => {
-        session.send('Hi! I\'m the help desk bot and I can help you create a ticket.');
-        builder.Prompts.text(session, 'First, please briefly describe your problem to me.');
+        var category = builder.EntityRecognizer.findEntity(args.intent.entities, 'category');
+        var severity = builder.EntityRecognizer.findEntity(args.intent.entities, 'severity');
+    
+        if (category && category.resolution.values.length > 0) {
+            session.dialogData.category = category.resolution.values[0];
+        }
+    
+        if (severity && severity.resolution.values.length > 0) {
+            session.dialogData.severity = severity.resolution.values[0];
+        }
+    
+        session.dialogData.description = session.message.text;
+    
+        if (!session.dialogData.severity) {
+            var choices = ['high', 'normal', 'low'];
+            builder.Prompts.choice(session, 'Which is the severity of this problem?', choices, { listStyle: builder.ListStyle.button });
+        } else {
+            next();
+        }
     },
     (session, result, next) => {
-        session.dialogData.description = result.response;
-        var choices = ['high', 'normal', 'low'];
-        builder.Prompts.choice(session, 'Which is the severity of this problem?', choices, { listStyle: builder.ListStyle.button});
+        if (!session.dialogData.severity) {
+            session.dialogData.severity = result.response.entity;
+        }
+    
+        if (!session.dialogData.category) {
+            builder.Prompts.text(session, 'Which would be the category for this ticket (software, hardware, network, and so on)?');
+        } else {
+            next();
+        }
     },
     (session, result, next) => {
-        session.dialogData.severity = result.response.entity;
-        builder.Prompts.text(session, 'Which would be the category for this ticket (software, hardware, networking, security, or other)?');
-    },
-    (session, result, next) => {
-        session.dialogData.category = result.response;
-        var message = `Great! I'm going to create a "${session.dialogData.severity}" severity ticket in "${session.dialogData.category}" category. ` + `The description I will use is "${session.dialogData.description}". Can you please confirm that this information is correct?`;
+        if (!session.dialogData.category) {
+            session.dialogData.category = result.response;
+        }
+    
+        var message = `Great! I'm going to create a "${session.dialogData.severity}" severity ticket in the "${session.dialogData.category}" category. ` +
+                      `The description I will use is "${session.dialogData.description}". Can you please confirm that this information is correct?`;
+    
         builder.Prompts.confirm(session, message, { listStyle: builder.ListStyle.button });
     },
     (session, result, next) => {
@@ -72,7 +109,16 @@ var bot = new builder.UniversalBot(connector, [
             session.endDialog('Ok. The ticket was not created. You can start again if you want.');
         }
     }
-]);
+])
+.triggerAction({
+    matches: 'SubmitTicket'
+});
+
+var luisRecognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL).onEnabled(function(context, callback) {
+    var enabled = context.dialogStack().length === 0;
+    callback(null, enabled);
+});
+bot.recognizer(luisRecognizer);
 
 const createCard = (ticketId, data) => {
     var cardTxt = fs.readFileSync('./cards/ticket.json', 'UTF-8');
